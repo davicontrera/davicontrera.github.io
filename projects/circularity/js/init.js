da10b35543a0ec8b0faf7bdd6b2cf8d0a1008c14
davicontrera <daviencontreras60@gmail.com>
2021-06-28 15:41:54 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -34,7 +34,7 @@ var init = function (window) {
 
         // TODO 3 / 8 : Call the drawCircle() function 
 
-        for (var  loopsCompleted = 0; loopsCompleted < 10; loopsCompleted++) {
+        for (var  loopsCompleted = 0; loopsCompleted < 250; loopsCompleted++) {
             drawCircle();
         }
         ////////////////////////////////////////////////////////////
